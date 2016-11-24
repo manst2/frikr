4dae086814cbee65f939f041c41c3bd9a5f1b1b7
nestor rosas meza <datos_clases1@hotmail.com>
2016-11-24 12:43:11 -0500

    Add files via upload

---
+++ python/tuplas,funciones.py
@@ -0,0 +1,20 @@
+#tuplas
+tuplas = ("nestor","rosas","meza")
+print (tuplas[0:3:2])
+print (tuplas)
+print (tuplas[0])
+print (tuplas[0:2])
+
+#diccionarios
+
+diccionarios = {"clave0" : "Nestor","clave1" : "Rosas","clave2" : "Meza"}
+print (diccionarios["clave0"])
+diccionarios["clave2"] = "hola"
+print (diccionarios["clave2"])
+
+#funciones 
+
+def funcion(num1,num2):
+	print(num1 + num2)
+
+funcion (3,4)
