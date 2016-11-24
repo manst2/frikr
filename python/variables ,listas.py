b61e554e451e5a40204855e3abc91f69b5e2bfaa
nestor rosas meza <datos_clases1@hotmail.com>
2016-11-24 02:53:20 -0500

    Add files via upload

---
+++ python/variables ,listas.py
@@ -0,0 +1,28 @@
+#Python
+print("practica de Python y Django")
+
+#variable 
+ #num1 = 2
+ #num=3
+num1 = input("ingrese numero 1")#ingresado por usuario
+num2 = input("ingrese numero 2")#ingresado por usuario
+resultado = int(num1) + int(num2)
+print(resultado)
+
+#condicionales 
+ if num1 > num2:
+ 	print("el numero mayor es :"+num1)
+ else:
+ 	print("el numero mayor es :"+num2)
+
+#bucle while
+años = 1
+while años <=20:
+	print("tienes "+srt(años))
+	años = años + 1
+	
+#listas
+print("listas")
+lista = ["python", "html", "css"]
+print (lista[0:])#imprimir lista completa
+print (lista[1])
